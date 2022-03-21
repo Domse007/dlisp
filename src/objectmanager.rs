9b3baaea2a362edb076209aab5439b318a04857a
Domse007 <dominikkeller55@gmail.com>
2022-03-21 20:03:29 +0100

    removed errors

--- src/objectmanager.rs
+++ src/objectmanager.rs
@@ -3,11 +3,49 @@ use crate::{
     lispobject::{LispObject, LispType},
 };
 
+#[derive(Default, Debug)]
 pub struct Manager {
     frames: Vec<Frame>,
 }
 
-#[derive(Default)]
+impl Manager {
+    pub fn new_frame(&mut self) {
+        self.frames.push(Frame::default());
+    }
+
+    pub fn pop_frame(&mut self) {
+        self.frames.pop();
+    }
+
+    pub fn get_val(&mut self, name: LispObject) -> Option<LispObject> {
+        for frame in self.frames.iter().rev() {
+            if let Some(val) = frame.get_val(name.clone()) {
+                return Some(val);
+            }
+        }
+        None
+    }
+
+    pub fn set_val(&mut self, key: LispObject, value: LispObject) -> Option<()> {
+        // Loop through all frames and check if the current frame contains the
+        // variable. If it finds the variable: return.
+        for frame in self.frames.iter_mut().rev() {
+            if frame.set_val(key.clone(), value.clone()).is_some() {
+                return Some(());
+            }
+        }
+
+        // If we did't already return, there was no variable with that name and
+        // it gets inserted into the current frame.
+        if let Some(frame) = self.frames.last_mut() {
+            return frame.set_val_force(key, value);
+        }
+
+        None
+    }
+}
+
+#[derive(Default, Debug)]
 struct Frame {
     scoped_objects: Vec<LispObject>,
 }
@@ -57,6 +95,130 @@ impl Frame {
             }
         }
 
+        //self.scoped_objects.push(cons(&[name, new]).unwrap());
+
         None
+    }
+
+    pub fn set_val_force(&mut self, name: LispObject, new: LispObject) -> Option<()> {
+        self.scoped_objects.push(cons(&[name, new]).unwrap());
+        Some(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::panic;
+
+    use super::*;
+
+    fn test_frame_default() -> Frame {
+        let mut frame = Frame::default();
+        frame.push(
+            cons(&[
+                LispObject::new_with(LispType::Symbol(String::from("test")), false),
+                LispObject::new_with(LispType::Number(22.), false),
+            ])
+            .unwrap(),
+        );
+        frame.push(
+            cons(&[
+                LispObject::new_with(LispType::Symbol(String::from("other-var")), false),
+                LispObject::new_with(LispType::Bool(true), false),
+            ])
+            .unwrap(),
+        );
+        frame.push(
+            cons(&[
+                LispObject::new_with(LispType::Symbol(String::from("another-one")), false),
+                LispObject::new_with(LispType::Number(23.), false),
+            ])
+            .unwrap(),
+        );
+        frame
+    }
+
+    #[test]
+    fn test_frame_get_val() {
+        let frame = test_frame_default();
+        if let LispType::Bool(var) = frame
+            .get_val(LispObject::new_with(
+                LispType::Symbol(String::from("other-var")),
+                false,
+            ))
+            .unwrap()
+            .get_type()
+        {
+            assert!(var);
+        } else {
+            panic!("Value could not be found.");
+        }
+
+        if let LispType::Number(n) = frame
+            .get_val(LispObject::new_with(
+                LispType::Symbol(String::from("another-one")),
+                false,
+            ))
+            .unwrap()
+            .get_type()
+        {
+            assert_eq!(n, 23.);
+        } else {
+            panic!("Value could not be found.");
+        }
+    }
+
+    #[test]
+    fn test_frame_set_val_existing() {
+        let mut frame = test_frame_default();
+        frame.set_val(
+            LispObject::new_with(LispType::Symbol(String::from("other-var")), false),
+            LispObject::new_with(LispType::Bool(true), false),
+        );
+
+        if let LispType::Bool(b) = frame
+            .get_val(LispObject::new_with(
+                LispType::Symbol(String::from("other-var")),
+                false,
+            ))
+            .unwrap()
+            .get_type()
+        {
+            assert!(b);
+        } else {
+            panic!("Value could not be found.");
+        }
+    }
+
+    #[test]
+    fn test_frame_multiple_frames() {
+        let mut manager = Manager::default();
+        manager.new_frame();
+        manager.set_val(LispObject::symbol("test"), LispObject::nil());
+        manager.set_val(LispObject::symbol("other"), LispObject::nil());
+        manager.new_frame();
+        manager.set_val(
+            LispObject::symbol("second"),
+            LispObject::new_with(LispType::Number(1.), false),
+        );
+        assert_eq!(
+            manager.get_val(LispObject::symbol("other")).unwrap(),
+            LispObject::nil()
+        );
+    }
+
+    #[test]
+    fn test_frame_multiple_frames_out_of_scope() {
+        let mut manager = Manager::default();
+        manager.new_frame();
+        manager.set_val(LispObject::symbol("this"), LispObject::number(1.));
+        manager.new_frame();
+        manager.set_val(LispObject::symbol("test"), LispObject::number(2.));
+        assert_eq!(
+            manager.get_val(LispObject::symbol("test")),
+            Some(LispObject::number(2.))
+        );
+        manager.pop_frame();
+        assert_eq!(manager.get_val(LispObject::symbol("test")), None);
     }
 }
